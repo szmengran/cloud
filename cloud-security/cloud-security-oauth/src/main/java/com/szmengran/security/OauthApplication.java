93618344b6eaac240abe8ab6e4fd221307f74f66
limaoyuan <android_li@sina.cn>
2018-03-19 17:00:04 +0800

    init porject

--- cloud-security/cloud-security-oauth/src/main/java/com/szmengran/security/OauthApplication.java
+++ cloud-security/cloud-security-oauth/src/main/java/com/szmengran/security/OauthApplication.java
@@ -2,20 +2,12 @@ package com.szmengran.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @RestController
-public class OauthApplication implements WebMvcConfigurer{
+public class OauthApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(OauthApplication.class, args);
-	}
-	
-	@Override
-	public void addViewControllers(ViewControllerRegistry registry) {
-//		registry.addViewController("/login").setViewName("/templates/index.html");
-//		registry.addViewController("/oauth/confirm_access").setViewName("authorize");
 	}
 }
