47aed358f3caa03383c20d8ea35bc09c784f5d8d
limaoyuan <android_li@sina.cn>
2018-03-24 10:24:45 +0800

    response加入空值的过滤

--- cloud-engineering/cloud-engineering-order/src/main/java/com/szmengran/cloud/order/EngineeringOrderApplication.java
+++ cloud-engineering/cloud-engineering-order/src/main/java/com/szmengran/cloud/order/EngineeringOrderApplication.java
@@ -2,10 +2,21 @@ package com.szmengran.cloud.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
 
 @SpringBootApplication
 public class EngineeringOrderApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EngineeringOrderApplication.class, args);
 	}
+	
+	@Bean
+    public Jackson2ObjectMapperBuilder objectMapperBuilder() {
+        Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
+        builder.serializationInclusion(JsonInclude.Include.NON_NULL);
+        return builder;
+    }
 }
