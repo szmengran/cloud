d1641a08a162be0c0b65465d8ff32774d47409d1
limaoyuan <android_li@sina.cn>
2018-03-28 15:06:54 +0800

    配置中心

--- cloud-config/cloud-config-server/src/main/java/com/szmengran/config/ConfigServerApplication.java
+++ cloud-config/cloud-config-server/src/main/java/com/szmengran/config/ConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.szmengran.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableConfigServer
 @SpringBootApplication
+@EnableConfigServer
+@EnableDiscoveryClient
 public class ConfigServerApplication {
 
 	public static void main(String[] args) {
