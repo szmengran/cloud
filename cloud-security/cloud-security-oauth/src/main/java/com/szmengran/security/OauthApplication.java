34d7394187dd9c61f931a93ded26ea821cd94654
limaoyuan <android_li@sina.cn>
2018-04-03 19:32:17 +0800

    oauth & config & gateway & eureka

--- cloud-security/cloud-security-oauth/src/main/java/com/szmengran/security/OauthApplication.java
+++ cloud-security/cloud-security-oauth/src/main/java/com/szmengran/security/OauthApplication.java
@@ -1,9 +1,11 @@
 package com.szmengran.security;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @RestController
 public class OauthApplication{
 
