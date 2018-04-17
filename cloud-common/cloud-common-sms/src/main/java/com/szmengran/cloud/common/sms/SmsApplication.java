4d30e3f867706a711ed934a456f2e9a89ff847c2
limaoyuan <android_li@sina.cn>
2018-04-17 09:01:34 +0800

    docker 配置

--- cloud-common/cloud-common-sms/src/main/java/com/szmengran/cloud/common/sms/SmsApplication.java
+++ cloud-common/cloud-common-sms/src/main/java/com/szmengran/cloud/common/sms/SmsApplication.java
@@ -2,6 +2,7 @@ package com.szmengran.cloud.common.sms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
@@ -11,6 +12,7 @@ import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import com.fasterxml.jackson.annotation.JsonInclude;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 @EnableFeignClients
 @RefreshScope
 @ComponentScan(basePackages = {"com.suntak.exception.controller", "com.szmengran.cloud", "com.szmengran.logging.service"})
