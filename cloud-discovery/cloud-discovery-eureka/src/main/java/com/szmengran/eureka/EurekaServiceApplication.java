9a8d1a895bbd02dfacc7465bc16570391c6a2a8c
limaoyuan <android_li@sina.cn>
2018-06-08 09:24:11 +0800

    版本升级

--- cloud-discovery/cloud-discovery-eureka/src/main/java/com/szmengran/eureka/EurekaServiceApplication.java
+++ cloud-discovery/cloud-discovery-eureka/src/main/java/com/szmengran/eureka/EurekaServiceApplication.java
@@ -2,10 +2,12 @@ package com.szmengran.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @EnableEurekaServer
 @SpringBootApplication
+@RefreshScope
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
