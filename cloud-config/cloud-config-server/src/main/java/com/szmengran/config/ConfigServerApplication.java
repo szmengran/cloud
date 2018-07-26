2b05acbe88e3ad6544a613e466f1995f5e8001b8
limaoyuan <android_li@sina.cn>
2018-07-26 14:17:03 +0800

    配置中心版本升级

--- cloud-config/cloud-config-server/src/main/java/com/szmengran/config/ConfigServerApplication.java
+++ cloud-config/cloud-config-server/src/main/java/com/szmengran/config/ConfigServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
