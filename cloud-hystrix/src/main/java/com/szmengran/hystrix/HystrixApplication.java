609cbaa07664f7a73509c8f16b5ca2fa36c7de08
limaoyuan <android_li@sina.cn>
2018-08-06 16:11:19 +0800

    hystrix config

--- cloud-hystrix/src/main/java/com/szmengran/hystrix/HystrixApplication.java
+++ cloud-hystrix/src/main/java/com/szmengran/hystrix/HystrixApplication.java
@@ -1,44 +1,19 @@
 package com.szmengran.hystrix;
 
-import org.springframework.boot.WebApplicationType;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
-import com.szmengran.hystrix.stream.MockStreamServlet;
-
-@Configuration
-@ComponentScan
-@EnableAutoConfiguration
-@Controller
+@SpringBootApplication
+@EnableTurbineStream
+@RefreshScope
 @EnableHystrixDashboard
-public class HystrixApplication extends SpringBootServletInitializer {
+public class HystrixApplication{
 	
-	@RequestMapping("/")
-	public String home() {
-		return "forward:/hystrix";
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixApplication.class, args);
 	}
-
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(HystrixApplication.class).web(WebApplicationType.SERVLET);
-    }
-
-    public static void main(String[] args) {
-        new SpringApplicationBuilder(HystrixApplication.class).web(WebApplicationType.SERVLET).run(args);
-    }
-
-    @SuppressWarnings({ "rawtypes", "unchecked" })
-	@Bean
-    public ServletRegistrationBean mockStreamServlet() {
-        return new ServletRegistrationBean(new MockStreamServlet(), "/mock.stream");
-    }
 
 }
