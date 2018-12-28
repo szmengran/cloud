1769da2d4ced4f22cee471b120beb61d7f12f5fa
limaoyuan <android_li@sina.cn>
2018-12-28 12:46:41 +0800

    月度汇总报表

---
+++ cloud-suntak/cloud-suntak-microservices/src/main/java/com/suntak/cloud/microservices/payment/service/XfSumService.java
@@ -0,0 +1,24 @@
+package com.suntak.cloud.microservices.payment.service;
+
+import com.suntak.cloud.microservices.payment.entity.XfSum;
+
+/**
+ * @Package com.suntak.cloud.microservices.payment.service
+ * @Description: 员工月度消费汇总
+ * @date Dec 27, 2018 5:06:31 PM
+ * @author <a href="mailto:android_li@sina.cn">Joe</a>
+ */
+public interface XfSumService {
+
+	/**
+	 * 根据工号和月份查询员工的就餐统计
+	 * @param empno
+	 * @param companycode
+	 * @param month
+	 * @return
+	 * @throws Exception 
+	 * @author <a href="mailto:android_li@sina.cn">Joe</a>
+	 */
+	public XfSum findXfsumByEmpno(String empno, String companycode, String month) throws Exception;
+	
+}
