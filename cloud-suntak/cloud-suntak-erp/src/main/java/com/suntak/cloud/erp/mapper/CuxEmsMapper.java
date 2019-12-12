c74222b365b20ef94cacc373c18b398033751381
limaoyuan <android_li@sina.cn>
2019-12-12 16:46:02 +0800

    同步EBS

--- cloud-suntak/cloud-suntak-erp/src/main/java/com/suntak/cloud/erp/mapper/CuxEmsMapper.java
+++ cloud-suntak/cloud-suntak-erp/src/main/java/com/suntak/cloud/erp/mapper/CuxEmsMapper.java
@@ -1,5 +1,7 @@
 package com.suntak.cloud.erp.mapper;
 
+import java.util.Map;
+
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Param;
@@ -13,17 +15,17 @@ import org.apache.ibatis.type.JdbcType;
 public interface CuxEmsMapper {
 
 	@Select("{CALL apps.CUX_EMS_WEB_SERVICE_PKG.PRO_MISCELL_TRANSACTION("
-			+ "#{retcode, mode=OUT, jdbcType=INTEGER},"
-			+ "#{errbuf, mode=OUT, jdbcType=VARCHAR},"
-			+ "#{p_org_id, mode=IN, jdbcType=INTEGER},"
-			+ "#{p_Header_Id, mode=IN, jdbcType=BIGINT},"
-            + "#{x_ebs_number, mode=OUT, jdbcType=VARCHAR})}")
+			+ "#{map.retcode, mode=OUT, jdbcType=INTEGER},"
+			+ "#{map.errbuf, mode=OUT, jdbcType=VARCHAR},"
+			+ "#{map.p_org_id, mode=IN, jdbcType=INTEGER},"
+			+ "#{map.p_Header_Id, mode=IN, jdbcType=BIGINT},"
+            + "#{map.x_ebs_number, mode=OUT, jdbcType=VARCHAR})}")
 	@Results({
 		@Result(column="retcode", property="retcode", jdbcType= JdbcType.INTEGER),
 		@Result(column="errbuf", property="errbuf", jdbcType= JdbcType.VARCHAR),
 		@Result(column="x_ebs_number", property="x_ebs_number", jdbcType= JdbcType.VARCHAR)
 	})
 	@Options(statementType = StatementType.CALLABLE)
-	void submitEBS(@Param("retcode") Integer retcode, @Param("errbuf") String errbuf, @Param("p_org_id") Integer p_org_id, @Param("p_Header_Id") Long p_Header_Id, @Param("x_ebs_number") String x_ebs_number);
+	void submitEBS(@Param("map") Map<String, Object> map);
 	
 }
--- cloud-suntak/cloud-suntak-erp/src/main/java/com/suntak/cloud/erp/service/impl/CuxEmsServiceImpl.java
+++ cloud-suntak/cloud-suntak-erp/src/main/java/com/suntak/cloud/erp/service/impl/CuxEmsServiceImpl.java
@@ -1,5 +1,8 @@
 package com.suntak.cloud.erp.service.impl;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +18,17 @@ public class CuxEmsServiceImpl implements CuxEmsService {
 	
 	@Override
 	public EbsResponse submitEBS(Integer p_org_id, Long p_Header_Id) {
-		Integer retcode = null;
-		String errbuf = null;
-		String x_ebs_number = null;
-		cuxEmsMapper.submitEBS(retcode, errbuf, p_org_id, p_Header_Id, x_ebs_number);
+		Map<String, Object> map = new HashMap<>();
+		map.put("retcode", null);
+		map.put("errbuf", null);
+		map.put("x_ebs_number", null);
+		map.put("p_org_id", p_org_id);
+		map.put("p_Header_Id", p_Header_Id);
+		cuxEmsMapper.submitEBS(map);
 		EbsResponse ebsResponse = new EbsResponse();
-		ebsResponse.setErrbuf(errbuf);
-		ebsResponse.setRetcode(retcode);
-		ebsResponse.setX_ebs_number(x_ebs_number);
+		ebsResponse.setErrbuf((String)map.get("errbuf"));
+		ebsResponse.setRetcode((Integer)map.get("retcode"));
+		ebsResponse.setX_ebs_number((String)map.get("x_ebs_number"));
 		return ebsResponse;
 	}
 
